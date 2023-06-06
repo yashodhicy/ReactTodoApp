ff6fdaf83796f2019056843c36cce765dad29186
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-06-06 21:46:29 +0300

    add svg icons for buttons

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import PropTypes  from "prop-types";
+import { FaPlusCircle } from "react-icons/fa"
+
 
 const InputTodo = ({ addTodoItem }) => {
     const [title, setTitle] = useState('');
@@ -29,7 +31,7 @@ const InputTodo = ({ addTodoItem }) => {
             onChange={handleChange} 
             className="input-text"
             />
-            <button className="input-submit">Submit</button>
+            <button className="input-submit"><FaPlusCircle /></button>
         </form>
         <span className="submit-warning">{message}</span>
         </>
